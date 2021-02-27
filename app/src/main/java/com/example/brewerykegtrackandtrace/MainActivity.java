3269c9d9e07632f1214f800103da17e1529ba574
Devashish <devashishkprasad@gmail.com>
2021-02-27 19:19:48 +0530

    first

---
+++ app/src/main/java/com/example/brewerykegtrackandtrace/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.brewerykegtrackandtrace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Thread welcomeThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(10000);  // Delay of 10 seconds
+                } catch (Exception e) {
+
+                } finally {
+
+                    Intent i = new Intent(MainActivity.this,
+                            login.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
+    }
+}
---
+++ app/src/main/java/com/example/brewerykegtrackandtrace/truckDriver.java
@@ -0,0 +1,14 @@
+package com.example.brewerykegtrackandtrace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class truckDriver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_truck_driver);
+    }
+}
