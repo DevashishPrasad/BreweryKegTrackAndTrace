7b427b8cb9d15c320bab57e52fc168d03e53c512
Devashish <devashishkprasad@gmail.com>
2021-03-03 22:55:14 +0530

    Login flow

--- app/src/main/java/com/example/brewerykegtrackandtrace/Login.java
+++ app/src/main/java/com/example/brewerykegtrackandtrace/Login.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
@@ -17,7 +19,18 @@ public class Login extends AppCompatActivity {
     }
 
     public void loginAndGo(View view){
-        Intent intent = new Intent(Login.this,SelectTruck.class);
+        // Get Phone and Password
+        String phone = ((EditText)findViewById(R.id.editTextPhone)).getText().toString();
+        String password = ((EditText)findViewById(R.id.editTextTextPassword)).getText().toString();;
+
+        // TODO Check the phone and password from database and fetch username
+        // TODO Populate this fetched data into static object
+
+        User user = new User();
+
+        Intent intent = new Intent(Login.this, SelectTruck.class);
+        intent.putExtra("Phone", phone);
+        // TODO intent.putExtra("Username", username);
         startActivity(intent);
     }
 
---
+++ app/src/main/java/com/example/brewerykegtrackandtrace/User.java
@@ -0,0 +1,10 @@
+package com.example.brewerykegtrackandtrace;
+
+public class User {
+    public static String username;
+    public static String truckno;
+    public static String location;
+    public static String loadunload;
+    public static String automanual;
+    public static String reportpermission;
+}
