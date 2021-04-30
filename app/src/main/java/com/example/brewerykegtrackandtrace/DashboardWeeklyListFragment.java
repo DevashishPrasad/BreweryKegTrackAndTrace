8576badf8a0743c89eddbae91c7a35e63563ebf9
Ayan Naresh Gadpal <ayangadpal2@gmail.com>
2021-05-01 01:33:25 +0530

    Dashboard Trans Started

--- app/src/main/java/com/example/brewerykegtrackandtrace/DashboardWeeklyListFragment.java
+++ app/src/main/java/com/example/brewerykegtrackandtrace/DashboardWeeklyListFragment.java
@@ -4,23 +4,22 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DashboardWeeklyListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.HashMap;
+import java.util.Iterator;
+
 public class DashboardWeeklyListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +27,6 @@ public class DashboardWeeklyListFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment weeklyListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static DashboardWeeklyListFragment newInstance(String param1, String param2) {
         DashboardWeeklyListFragment fragment = new DashboardWeeklyListFragment();
         Bundle args = new Bundle();
@@ -55,10 +45,80 @@ public class DashboardWeeklyListFragment extends Fragment {
         }
     }
 
+    LinearLayout transaction_daily_location,transaction_daily_truck,kegLL,loadCountLL,unloadCountLL;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weekly_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_weekly_list, container, false);
+
+        transaction_daily_location = view.findViewById(R.id.transaction_weekly_location);
+        transaction_daily_truck = view.findViewById(R.id.transaction_weekly_truck);
+        loadCountLL = view.findViewById(R.id.transaction_weekly_load);
+        unloadCountLL = view.findViewById(R.id.transaction_weekly_unload);
+        kegLL = view.findViewById(R.id.transaction_weekly_keg);
+        updateUI();
+
+        return view;
     }
+
+    private void updateUI() {
+
+        Iterator it = User.dataHolder_weekly.entrySet().iterator();
+
+        while (it.hasNext()) {
+            HashMap.Entry pair = (HashMap.Entry)it.next();
+
+            TransKegType k = (TransKegType) pair.getValue();
+
+            TextView k30_l = setTextView(String.valueOf(k.k30_l));
+            TextView k30_un = setTextView(String.valueOf(k.k30_un));
+
+            TextView k50_l = setTextView(String.valueOf(k.k50_l));
+            TextView k50_un = setTextView(String.valueOf(k.k50_un));
+
+            TextView co2_l = setTextView(String.valueOf(k.co2_l));
+            TextView co2_un = setTextView(String.valueOf(k.co2_un));
+
+            TextView disp_l = setTextView(String.valueOf(k.disp_l));
+            TextView disp_un = setTextView(String.valueOf(k.disp_un));
+
+
+            for (int i = 0;i<4;i++)
+            {
+                transaction_daily_location.addView(setTextView((String) pair.getKey()));
+                transaction_daily_truck.addView(setTextView(k.t_trans_rn));
+            }
+
+            kegLL.addView(setTextView("k30"));
+            loadCountLL.addView(k30_l);
+            unloadCountLL.addView(k30_un);
+
+            kegLL.addView(setTextView("k50"));
+            loadCountLL.addView(k50_l);
+            unloadCountLL.addView(k50_un);
+
+
+            kegLL.addView(setTextView("CO2"));
+            loadCountLL.addView(co2_l);
+            unloadCountLL.addView(co2_un);
+
+            kegLL.addView(setTextView("Disp"));
+            loadCountLL.addView(disp_l);
+            unloadCountLL.addView(disp_un);
+
+            it.remove();
+        }
+    }
+
+    public TextView setTextView(String text)
+    {
+        TextView tv = new TextView(getContext().getApplicationContext());
+        tv.setTextSize(TypedValue.COMPLEX_UNIT_SP,14);
+        tv.setText(text);
+
+        return tv;
+    }
+
 }
